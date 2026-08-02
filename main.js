aa7bcf9fe68401a13d2a15f269e754d932c326a6
Soapie <104187938+CombatWwombat@users.noreply.github.com>
2026-08-02 13:28:21 +0100

    Update main.js

--- main.js
+++ main.js
@@ -45,11 +45,10 @@ function init() {
       data.planets.forEach(p => {
         const geo = new THREE.SphereGeometry(p.radius, 32, 32);
         
-        const mat = new THREE.MeshBasicMaterial({ 
+        const mat = new THREE.MeshStandardMaterial({ 
           color: p.color, 
           transparent: true, 
-          opacity: 0.5,
-          depthTest: false
+          opacity: 0.5
         });
         
         const mesh = new THREE.Mesh(geo, mat);
